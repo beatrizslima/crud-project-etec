12890c657b1ba6bf108f54020c391aed0d90b77c
beatrizslima <bialima.ricarte@gmail.com>
2023-02-13 22:19:10 -0300

    New pages and database changes

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+
+        <!--Bootstrap-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Cadastro Funcionários</title>
+</head>
+
+<body>
+    <div class="container p-3 my-3 border">
+    <h2>Bem vindo ao nosso cadastro de funcionários.</h2>
+        <div class= "image-container">
+        <img src="assets/people.png" alt="Pessoas" width="800px" class="mx-auto d-block">
+        </div>
+    <p>Selecione a opção que deseja:</p>
+
+            <button class="btn btn-outline-warning">Novo funcionário</button>
+            <button class="btn btn-outline-warning">Lista de funcionários</button>
+
+    </div> 
+
+</body>
+</html>
